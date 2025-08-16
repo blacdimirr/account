31220e8446041b9abebc9bd81e10e8ef66c3249f
Wilbren Rosario <wilbrenrosario@gmail.com>
2025-08-16 10:10:47 -0400

    Selector de productos

--- resources/views/bill/create.blade.php
+++ resources/views/bill/create.blade.php
@@ -651,7 +651,7 @@
                             <tbody class="ui-sortable" data-repeater-item>
                                 <tr>
                                     <td width="25%" class="form-group pt-0 flex-nowrap">
-                                        {{ Form::select('item', $product_services, '', ['class' => 'form-control item', 'data-url' => route('bill.product'), 'required' => 'required']) }}
+                                        {{ Form::select('item', $product_services, '', ['class' => 'form-control item select2', 'data-url' => route('bill.product'), 'required' => 'required']) }}
                                     </td>
                                     <td>
                                         <div class="form-group price-input input-group search-form flex-nowrap">
